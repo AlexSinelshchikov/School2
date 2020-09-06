7909779673e4d4988354661a484401cea285430d
Алексей <Alexsinelshikov2303@gmail.com>
2020-09-06 23:37:09 +0800

    переместил gitignore

--- src/TenthTask/TenthTask.java
+++ src/TenthTask/TenthTask.java
@@ -7,14 +7,14 @@ public class TenthTask {
         int a = array.length;
         EndZero(array, a);
 
-        for (int i : array) System.out.print(i + " ");
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
     }
     static void EndZero(int[] array, int a) {
         int count = 0;
         for (int i = 0; i < a; i++)
-            if (array[i] != 0) {
-                array[count++] = array[i];
-            }
+            if (array[i] != 0) array[count++] = array[i];
         while (count < a)
             array[count++] = 0;
     }
